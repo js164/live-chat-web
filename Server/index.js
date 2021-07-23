b5989cebe2fdf8442a8fcfc174d4bfce35f392f0
js164 <67049518+js164@users.noreply.github.com>
2021-07-23 22:47:08 +0530

    Add files via upload

--- Server/index.js
+++ Server/index.js
@@ -4,6 +4,34 @@ const io = require('socket.io')( process.env.PORT || 8000, {
     }
   });
 
+var path = require('path');
+
+const express=require('express')
+const app=express()
+const route=express.Router()
+const port= process.env.PORT || 3000
+
+route.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'/index.html'))
+})
+route.get('/client.js',(req,res)=>{
+    res.sendFile(path.join(__dirname,'/client.js'))
+})
+route.get('/client.css',(req,res)=>{
+    res.sendFile(path.join(__dirname,'/client.css'))
+})
+route.get('/Message_notification.mp3',(req,res)=>{
+    res.sendFile(path.join(__dirname,'/Message_notification.mp3'))
+})
+route.get('/wallpaper.png',(req,res)=>{
+    res.sendFile(path.join(__dirname,'/wallpaper.png'))
+})
+
+app.use('/', route);
+app.listen(port,()=>{
+    console.log(`starting Server`)
+})
+
 const users = {};
 
 io.on('connection', socket =>{
